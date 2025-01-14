6133ec161ebb2eefc06afcf3f8f91ee40b1b1f0e
mshns <mishanos412@gmail.com>
2025-01-14 13:25:10 +0300

    feat: add camera widgets

--- frames/run/scripts.js
+++ frames/run/scripts.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const subtitleList = document.querySelector(".subtitle-list");
-  const items = document.querySelectorAll(".subtitle-list .subtitle-item");
+document.addEventListener('DOMContentLoaded', () => {
+  const subtitleList = document.querySelector('.subtitle-list');
+  const items = document.querySelectorAll('.subtitle-list .subtitle-item');
   let currentIndex = 0;
   const transitionInDuration = 1000; // 1 секунда для расширения
   const transitionOutDuration = 2000; // 2 секунды для сужения
@@ -9,33 +9,29 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!subtitleList || items.length === 0) return;
 
   // Установить первый элемент как видимый
-  items[currentIndex].classList.add("visible");
+  items[currentIndex].classList.add('visible');
 
   function animate() {
     // Расширение до 100%
     subtitleList.style.transition = `width ${transitionInDuration}ms ease-in-out`;
-    subtitleList.style.width = "100%";
-
+    subtitleList.style.width = '100%';
     setTimeout(() => {
       // Задержка на показ элемента
       setTimeout(() => {
         // Сужение до 0%
         subtitleList.style.transition = `width ${transitionOutDuration}ms ease-in-out`;
-        subtitleList.style.width = "0%";
-
+        subtitleList.style.width = '0%';
         // После завершения анимации переключаем элемент
         setTimeout(() => {
-          items[currentIndex].classList.remove("visible");
+          items[currentIndex].classList.remove('visible');
           currentIndex = (currentIndex + 1) % items.length;
-          items[currentIndex].classList.add("visible");
-
+          items[currentIndex].classList.add('visible');
           // Перезапуск анимации
           animate();
         }, transitionOutDuration);
       }, displayDuration);
     }, transitionInDuration);
   }
-
   // Запуск анимации
   animate();
 });
